487aab109668998ee46e1e65288ea1450ab5d497
huhuanming <workboring@gmail.com>
2017-11-15 00:54:39 +0800

    Initial commit

---
+++ Example/UploadKit/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UploadKit
+//
+//  Created by huhuanming on 11/14/2017.
+//  Copyright (c) 2017 huhuanming. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+  }
+}
---
+++ Example/UploadKit/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UploadKit
+//
+//  Created by huhuanming on 11/14/2017.
+//  Copyright (c) 2017 huhuanming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
